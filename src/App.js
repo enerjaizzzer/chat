fafc7ca5aecc90ad4b9b7ab58f815a3f05c15a9e
enerjaizzzer <enerjaizzzer@gmail.com>
2019-08-08 18:29:32 +0300

    feat: create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
+
 class App extends Component {
   render() {
     return (
       <>
-      {/* <Header />
+      <Header />
       <Main />
-      <Footer /> */}
+      <Footer />
       </>
     );
   }
