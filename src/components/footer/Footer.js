9469ddd7d43e467cacf249a8e2d110412dbdde2c
enerjaizzzer <enerjaizzzer@gmail.com>
2019-08-12 14:54:11 +0300

    feat: add footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  Navbar,
+  Button,
+  Form,
+  Col,
+} from 'react-bootstrap';
 
-const Footer = () => (
-  <>
-    <h1>Footer</h1>
-  </>
-);
+class Footer extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" className="fixed-bottom">
+
+        <Col xs={11}>
+          <Form
+            className="text-center"
+            onSubmit={this.submitValue}
+            md={10}
+          >
+            <Form.Control
+              type="text"
+              onChange={this.onChangePassword}
+              placeholder="Text message"
+            />
+          </Form>
+        </Col>
+        <Col xs={1}>
+          <Button
+            variant="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Col>
 
 
-export default Footer;
+
+      </Navbar>
+    );
+  }
+}
+
+export default connect(state => ({
+  userInformation: state.settingUser.users[state.settingUser.loginUser],
+}))(Footer);
